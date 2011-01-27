8319d7e483c5ff3260ec78982d8c162afa549db6
Francesco Strozzi <francesco.strozzi@gmail.com>
2011-01-27 17:56:12 +0100

    fixed BWA#aln and BWA#samse methods

--- lib/bio-bwa.rb
+++ lib/bio-bwa.rb
@@ -47,15 +47,19 @@ module Bio
         self.call_function(args)
       end
       
-      def self.aln(index_prefix,file_in,params={:n => 0.04,:o => 1,:e => -1,:i => 5, :d => 10,:l => 32,:k => 2,:m => 2000000,:t => 1,:M => 3,:O => 11,:E => 4,:R => 30,:q => 0,:f => nil,:B => nil,:c => false,:L=>false,:N=>false,:I=>false,:b=>false,:single => false,:first => false,:second => false})
+      def self.aln(index_prefix,file_in,params={}) #params={:n => 0.04,:o => 1,:e => -1,:i => 5, :d => 10,:l => 32,:k => 2,:m => 2000000,:t => 1,:M => 3,:O => 11,:E => 4,:R => 30,:q => 0,:f => nil,:B => nil,:c => false,:L=>false,:N=>false,:I=>false,:b=>false,:single => false,:first => false,:second => false}
           args = ["aln"]
+          valid_params = %w(n o e i d l k c L R m t N M O E q f b single first second I B)
           params.each_key do |k|
+            raise ArgumentError, "Unknown parameter '#{k}' for BWA#aln method!" if !valid_params.include?(k.to_s)
             if params[k] == true
               case k
                 when :single then args << "-0"
                 when :first then args << "-1"
                 when :second then args << "-2"
-              end    
+              else
+                args << "-#{k}"
+              end 
             elsif params[k]
               args << "-#{k}"
               args << params[k]
@@ -66,7 +70,22 @@ module Bio
           self.call_function(args)
       end
       
-      ######## Methods to manipulate C arguments and functions ########
+      
+      def self.samse(index_prefix,sai_in,fasta_in,params = {}) # params = {:n => 3, :r => nil, :f => "out.sam"}
+        args = ["sai2sam_se"]
+        valid_params = %w(n r f)
+        params.each_key do |k|
+          raise ArgumentError, "Unknown parameter '#{k}' for BWA#samse method!" if !valid_params.include?(k.to_s)
+          args << "-#{k}"
+          args << params[k]
+        end
+        args << index_prefix
+        args << sai_in
+        args << fasta_in
+        self.call_function(args)
+      end
+      
+      ######## Methods to handle C functions and arguments ########
       
       attach_function :bwa_fa2pac, [:int,:pointer], :int
       attach_function :bwa_pac2bwt, [:int,:pointer], :int
@@ -75,19 +94,21 @@ module Bio
       attach_function :bwa_bwt2sa, [:int,:pointer], :int
       attach_function :bwa_index, [:int,:pointer], :int
       attach_function :bwa_aln, [:int,:pointer], :int
+      attach_function :bwa_sai2sam_se, [:int, :pointer], :int
+      
       
       def self.call_function(args)
         begin
           c_args = self.build_args(args)
           self.send("bwa_#{args[0]}".to_sym,args.size,c_args)
         rescue 
-          raise NoMethodError, "BWA Function '"+args[0]+"' not found!"
+          raise NoMethodError, "BWA Function '"+args[0]+"' not found!" 
         end
       end 
       
       def self.build_args(args)
         cmd_args = args.map do |arg|
-          FFI::MemoryPointer.from_string(arg)
+          FFI::MemoryPointer.from_string(arg.to_s)
         end
         exec_args = FFI::MemoryPointer.new(:pointer, cmd_args.length)
         cmd_args.each_with_index do |arg, i|
