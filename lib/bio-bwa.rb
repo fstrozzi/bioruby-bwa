d5993a3b83e70c8c0ca538865af9edcb0cb7151b
Francesco Strozzi <francesco.strozzi@gmail.com>
2011-02-02 18:15:17 +0100

    fixed Linux library extension and added 64bit control

--- lib/bio-bwa.rb
+++ lib/bio-bwa.rb
@@ -4,3 +4,10 @@ require 'bio/library'
 require 'bio/bwa'
 
 
+# checking if system architecture is 32bit or 64bit
+if Bio::BWA::Library.arch_type == "32Bit" then
+  raise SystemError, "Unable to run. BWA library is compiled for 64Bit systems!"
+end
+  
+
+
--- lib/bio/library.rb
+++ lib/bio/library.rb
@@ -1,13 +1,18 @@
 module Bio
   class BWA
     class Library
+      
+      def self.arch_type
+        ['a'].pack('P').length  > 4 ? "64Bit" : "32Bit"
+      end
+      
       def self.filename
-        lib_os = case RUBY_PLATFORM
-          when /linux/ then 'a'
+        lib_extension = case RUBY_PLATFORM
+          when /linux/ then 'so'
           when /darwin/ then 'dylib'
           when /windows/ then raise NotImplementedError, "BWA software is not available for Windows platform"  
         end
-        File.join(File.expand_path(File.dirname(__FILE__)),'ext',"libbwa.#{lib_os}")
+        File.join(File.expand_path(File.dirname(__FILE__)),'ext',"libbwa.#{lib_extension}")
       end
     end
   end
