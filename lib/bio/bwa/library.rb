3a1e6af2fe4d69dfcbf6bf011057a87a88e067c4
Francesco Strozzi <francesco.strozzi@gmail.com>
2011-02-09 11:55:15 +0100

    added yardoc comments for BWA::Library

--- lib/bio/bwa/library.rb
+++ lib/bio/bwa/library.rb
@@ -4,6 +4,9 @@ module Bio
       
       require 'rbconfig'      
       
+      # Load the correct library for the OS system in use
+      # @return [String] the absolute path for the filename of the shared library
+      # @note this method is called automatically when the module is loaded
       def self.load
         raise RuntimeError,"Unable to load shared library! BWA is compiled for 64bit systems" if arch_type == "32bit"
         lib_extension = case Config::CONFIG['host_os']
@@ -16,6 +19,9 @@ module Bio
       
       private
       
+      # Chech if the system architecture is 32bit or 64bit
+      # @return [String] architecture type (64bit or 32bit)
+      # @note this method is used internally
       def self.arch_type
         if RUBY_PLATFORM == 'java'
           return (Config::CONFIG['target_cpu'] == "x86_64") ? "64bit" : "32bit" 
